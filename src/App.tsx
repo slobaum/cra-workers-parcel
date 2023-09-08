b279516bde0f8b8302d6c1483f88918261e4199b
Daniel Carter <slobaum@gmail.com>
2023-09-08 21:22:49 +0200

    example interface with worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useCallback, useEffect, useRef } from 'react';
 
-function App() {
+const App: FC = () => {
+  const workerRef = useRef<Worker>();
+
+  const sendMessageToWorker = useCallback(() => {
+    workerRef.current?.postMessage({});
+  }, []);
+
+  useEffect(() => {
+    workerRef.current = new Worker("/workers/sampleWorker.js");
+
+    workerRef.current?.addEventListener('message', (event) => {
+      alert('message received from worker: ' + JSON.stringify(event.data));
+    });
+
+    return () => workerRef.current?.terminate();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={sendMessageToWorker}>
+        Send message to worker
+      </button>
     </div>
   );
 }
